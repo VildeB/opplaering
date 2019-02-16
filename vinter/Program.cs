18218fe301c6fac831da8367fac30b87c0694a48
Harald Bjorøy <harald.bjoroy@spv.no>
2019-02-16 21:44:12 +0100

    Lagt til programmet frå førre gang

---
+++ vinter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace vinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var antall = 10;
+            var noe = true;
+            for (int i = 0; i < antall; i++, noe = !noe)
+            {
+                if (noe)
+                {
+                    Console.WriteLine("Hilu Verden! {0}-{1} {2}",antall,i,noe);
+                }
+                else
+                {
+                    Console.WriteLine("Hello World! {0}-{1} {2}",antall,i,noe);
+                }
+                   
+            }
+        }
+    }
+}
