045af562db090e6bd98e5863f6f0c0ae24488375
Vilde Bjorøy <vilde@kven.no>
2019-02-19 21:39:40 +0100

    Piltaster

--- vinter/Program.cs
+++ vinter/Program.cs
@@ -7,49 +7,57 @@ namespace vinter
         static void Main(string[] args)
         {
             //const string blå = "\u001b[34m";
-            const string cyan = "\u001b[36m";
-            const string raud = "\u001b[31m";
+            //const string cyan = "\u001b[36m";
+            //const string raud = "\u001b[31m";
             // const string gul = "\u001b[33m";
             //const string kvit = "\u001b[37m";
             const string reset= "\u001b[0m";
+            const string uparrow = "\u001b[1A";
+            const string downarrow = "\u001b[1B";
+            const string rightarrow = "\u001b[1C";
+            const string leftarrow = "\u001b[1D";
 
-            var antall = 10;
-            var noe = true;
 
             ConsoleTools.EnableAnsi();
-            for (int c = 0; c < antall; c++, noe = !noe)
-            {
-                for (int i = 0; i < antall; i++, noe = !noe)
-                {
-                    if (noe)
-                    {
-                        Console.Write("{0}aa",raud);
-                    }
-                    else
-                    {
-                        Console.Write("{0}bb",cyan);
-                    }
+            ConsoleKeyInfo key;
+            // ConsoleKey.DownArrow; ConsoleKey.UpArrow; ConsoleKey.LeftArrow; ConsoleKey.RightArrow
 
+            do {
+
+                key=Console.ReadKey(true);
+
+                if (
+                    key.Key == ConsoleKey.DownArrow || 
+                    key.Key == ConsoleKey.UpArrow || 
+                    key.Key == ConsoleKey.LeftArrow || 
+                    key.Key == ConsoleKey.RightArrow
+                    ) 
+                {
+                        switch (key.Key) 
+                        {
+                            case ConsoleKey.DownArrow:
+                                Console.Write (downarrow);
+                                break;
+                            case ConsoleKey.UpArrow:
+                                Console.Write (uparrow);
+                                break;
+                            case ConsoleKey.RightArrow:
+                                Console.Write (rightarrow);
+                                break;
+                            case ConsoleKey.LeftArrow:
+                                Console.Write (leftarrow);
+                                break;
+                        }
+                        Console.Write ("*{0}",leftarrow);
+                    
                 }
-                Console.WriteLine();
-            }
-            
+             
+
+
+
+            } while (key.Key != ConsoleKey.E);
+
             Console.WriteLine(reset);
         }
     }
 }
-
-/*
-       //    var ord =  Console.ReadLine();
-
-
-               if (noe==true)
-                {
-                    Console.Write("{3}{4} {0}-{1} {2}",antall,i,noe,cyan,ord);
-                }
-                else
-                {
-                    Console.Write("{3}Hello World! {0}-{1} {2}",antall,i,noe,raud);
-                }
-
- */
