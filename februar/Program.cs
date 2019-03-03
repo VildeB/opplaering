bc6beb6856b9a4f27f129b531377382527904714
Vilde Bjorøy <vilde@kven.no>
2019-03-03 14:36:45 +0100

    oppgave løst

---
+++ februar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace februar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.A) 
+            {
+                Console.WriteLine("Du har trykket A");
+            }
+            else
+            {
+                Console.WriteLine("Du har ikke trykket A");
+            }
+
+       }
+    }
+}
--- vinter/Program.cs
+++ vinter/Program.cs
@@ -7,8 +7,8 @@ namespace vinter
         static void Main(string[] args)
         {
             //const string blå = "\u001b[34m";
-            //const string cyan = "\u001b[36m";
-            //const string raud = "\u001b[31m";
+            const string cyan = "\u001b[36m";
+            const string raud = "\u001b[31m";
             // const string gul = "\u001b[33m";
             //const string kvit = "\u001b[37m";
             const string reset= "\u001b[0m";
@@ -17,15 +17,22 @@ namespace vinter
             const string rightarrow = "\u001b[1C";
             const string leftarrow = "\u001b[1D";
 
-
+            
             ConsoleTools.EnableAnsi();
+            Console.WriteLine ();
+            for (var i = 0; i<25;i++) {
+                Console.Write (uparrow);
+            }
+            for (var i = 0; i<25;i++) {
+                Console.WriteLine ("                                                                                                                                 ");
+            }
             ConsoleKeyInfo key;
             // ConsoleKey.DownArrow; ConsoleKey.UpArrow; ConsoleKey.LeftArrow; ConsoleKey.RightArrow
-
+            var farge = 0;
             do {
 
                 key=Console.ReadKey(true);
-
+                
                 if (
                     key.Key == ConsoleKey.DownArrow || 
                     key.Key == ConsoleKey.UpArrow || 
@@ -48,8 +55,25 @@ namespace vinter
                                 Console.Write (leftarrow);
                                 break;
                         }
+                        farge = !farge;
+                       /*  if (farge )
+                        {
+                            Console.Write (cyan);
+                        }
+                        else 
+                        {
+                            Console.Write (raud);
+                        }
                         Console.Write ("*{0}",leftarrow);
-                    
+                      
+...
+farge = (farge + 1) % 4;
+…
+
+switch (farge) {
+case 0: ...
+   break;
+                    */
                 }
              
 
